062d4afa90ab675bc59a4d7c6f20b9878c5f88b0
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-20 03:08:20 +0530

    Added : code for queu

--- src/Stack/MyStackTest.java
+++ src/Stack/MyStackTest.java
@@ -8,17 +8,17 @@ import org.junit.Test;
 public class MyStackTest {
 	@Test
 	public void given3NumberInStackWhenPopedShouldMatchWithLastAddedNode() {
-		Node<Integer> myFirstNode = new Node<>(70);
+		Node<Integer> myFirstNode = new Node<>(56);
 		Node<Integer> mySecondNode = new Node<>(30);
-		Node<Integer> myThirdNode = new Node<>(56);
+		Node<Integer> myThirdNode = new Node<>(70);
 		Stack myStack = new Stack();
-		myStack.push(myFirstNode);
-		myStack.push(mySecondNode);
-		myStack.push(myThirdNode);
-		INode pop = myStack.pop();
+		myStack.enqueu(myFirstNode);
+		myStack.enqueu(mySecondNode);
+		myStack.enqueu(myThirdNode);
 		INode peak = myStack.peak();
+		myStack.pop();
 		myStack.printStack();
-		Assert.assertEquals(myThirdNode, pop);
+		Assert.assertEquals(myFirstNode, peak);
 	}
 
 }
--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -23,4 +23,8 @@ public class Stack {
 		return myLinkedList.pop();
 		
 	}
+	public void enqueu(INode myNode) {
+		myLinkedList.appendNodes(myNode);
+		
+	}
 }
