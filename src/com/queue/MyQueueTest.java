2c48749574792ee2c6065e7d49e5f806442112d6
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-23 02:51:46 +0530

    Refactor : changed file and package

--- src/com/queue/MyQueueTest.java
+++ src/com/queue/MyQueueTest.java
@@ -7,7 +7,7 @@ package com.queue;
 
 	public class MyQueueTest {
 		@Test
-		public void given3NumberInStackWhenPopedShouldMatchWithLastAddedNode() {
+		public void given3NumberInQueuekWhenEnquedShouldAddedAfterTheAddedNode() {
 			Node<Integer> myFirstNode = new Node<>(56);
 			Node<Integer> mySecondNode = new Node<>(30);
 			Node<Integer> myThirdNode = new Node<>(70);
